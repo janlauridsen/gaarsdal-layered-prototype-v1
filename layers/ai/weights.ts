53d49de3c0e21ac4135cff81fa189f34e0c0fcf6
Jan Lauridsen <jan.lauridsen@hotmail.com>
2025-12-16 14:27:19 +0100

    Create weights.ts

---
+++ layers/ai/weights.ts
@@ -0,0 +1,16 @@
+import { DomainSignal } from "@/layers/domain/domain-types";
+
+export const SIGNAL_WEIGHTS: Record<DomainSignal, number> = {
+  [DomainSignal.NONE]: 0,
+
+  [DomainSignal.STRESS]: 1,
+  [DomainSignal.REGULATION_REQUEST]: 1,
+
+  [DomainSignal.AMBIGUOUS_VULNERABILITY]: 3,
+
+  [DomainSignal.TRAUMA_REFERENCE]: 5,
+  [DomainSignal.PTSD_REFERENCE]: 5,
+
+  [DomainSignal.SELF_HARM_SIGNAL]: 10,
+  [DomainSignal.SUICIDAL_IDEATION]: 10,
+};
